b76a924e17df429103039b6046f0b82c93dcc52e
Muhammad Rizky Fahreza Gusti <muhammad.120140071@student.itera.ac.id>
2023-01-25 00:12:09 +0700

    update BE

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+     require_once 'post-crud/functions/helpers.php';
+     require_once 'post-crud/functions/pdo_connection.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -88,7 +93,7 @@
             </ul>
           </li>
           <li><a class="nav-link scrollto" href="#contact">Kontak</a></li>
-          <li><a class="getstarted scrollto" href="#about">Admin</a></li>
+          <li><a class="getstarted scrollto" href='post-crud/auth/login.php'>Admin</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
@@ -169,21 +174,21 @@
             <div class="icon-box">
               <h4 class="title"><a href=""></a>Pendidikan</h4>
               <div class="icon"><i class="bi bi-laptop"></i></div>
-              <p class="description">Jadi yogi samahaendra adalah si anak yang tampan dan pemberani</p>
+              <p class="description">Jadi ntar fikri isi grafik terus dikasi penjelasan</p>
             </div>
           </div>
           <div class="col-lg-4 col-md-6">
             <div class="icon-box">
               <h4 class="title"><a href="">Penduduk</a></h4>
               <div class="icon"><i class="bi bi-briefcase"></i></div>
-              <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+              <p class="description">Jadi ntar fikri isi grafik terus dikasi penjelasan</p>
             </div>
           </div>
           <div class="col-lg-4 col-md-6">
             <div class="icon-box">
               <h4 class="title"><a href="">Pekerjaan</a></h4>
               <div class="icon"><i class="bi bi-calendar4-week"></i></div>
-              <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur trade stravi</p>
+              <p class="description">Jadi ntar fikri isi grafik terus dikasi penjelasan</p>
             </div>
           </div>
         </div>
@@ -352,6 +357,10 @@
 
     <!-- ======= Portfolio Section ======= -->
     <section id="portfolio" class="portfolio">
+      
+    
+
+                
       <div class="container">
 
         <div class="section-title">
@@ -369,138 +378,34 @@
             </ul>
           </div>
         </div>
-
-        <div class="row portfolio-container">
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>App 1</h4>
-                <p>App</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+            <?php
+                        $query = "SELECT * FROM posts WHERE status = 10";
+                        $statement = $pdo->prepare($query);
+                        $statement->execute();
+                        $posts = $statement->fetchAll();
+                        $numOfCols = 3;
+                        $rowCount = 0;
+                        $bootstrapColWidth = 12 / $numOfCols;
+                        foreach ($posts as $post) {
+                  if($rowCount % $numOfCols == 0) { ?> <div class="row portfolio-container"> <?php } 
+                    $rowCount++; ?>  
+                      <div class="col-md portfolio-item filter-web" >
+                        <div class="portfolio-wrap">
+                        <img src="<?= asset($post->image) ?>" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                          <h4><?= $post->title ?></h4>
+                          <p><?= substr($post->body, 0, 80) ?></p>
+                        <div class="portfolio-links">
+                          <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title= "<?= $post->title ?><"> <i class="bx bx-plus"></i></a>
+                          <a href="<?= url('detail.php?post_id=') . $post->id ?>" title="Details"><i class="bx bx-link"></i></a>
+                      </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Web 3</h4>
-                <p>Web</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>App 2</h4>
-                <p>App</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 2"><i class="bx bx-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Card 2</h4>
-                <p>Card</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Web 2</h4>
-                <p>Web</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 2"><i class="bx bx-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>App 3</h4>
-                <p>App</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 3"><i class="bx bx-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Card 1</h4>
-                <p>Card</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 1"><i class="bx bx-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Card 3</h4>
-                <p>Card</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 3"><i class="bx bx-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Web 3</h4>
-                <p>Web</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
         </div>
+<?php
+    if($rowCount % $numOfCols == 0) { ?> </div> <?php } }?>
+     
 
-      </div>
     </section><!-- End Portfolio Section -->
 
     <!-- ======= Team Section ======= -->
--- post-crud/index.php
+++ post-crud/index.php
@@ -8,13 +8,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bit Blog</title>
-    <link rel="stylesheet" href="<?= asset('assets/css/bootstrap.min.css') ?>" media="all" type="text/css">
-    <link rel="stylesheet" href="<?= asset('assets/css/style.css') ?>" media="all" type="text/css">
+    <link rel="stylesheet" href="<?= asset('post-crud/assets/css/bootstrap.min.css') ?>" media="all" type="text/css">
+    <link rel="stylesheet" href="<?= asset('post-crud/assets/css/style.css') ?>" media="all" type="text/css">
 </head>
 <body>
 <section id="app">
 
-    <?php require_once "layouts/top-nav.php"?>
+    <?php require_once "./layouts/top-nav.php"?>
 
     <section class="container my-5">
         <!-- Example row of columns -->
@@ -31,7 +31,7 @@
                     <section class="mb-2 overflow-hidden" style="max-height: 15rem;"><img class="img-fluid" src="<?= asset($post->image) ?>" alt=""></section>
                     <h2 class="h5 text-truncate"><?= $post->title ?></h2>
                     <p><?= substr($post->body, 0, 80) ?></p>
-                    <p><a class="btn btn-primary" href="<?= url('detail.php?post_id=') . $post->id ?>" role="button">View details »</a></p>
+                    <p><a class="btn btn-primary" href="<?= url('/detail.php?post_id=') . $post->id ?>" role="button">View details »</a></p>
                 </section>
 
                 <?php } ?>
